d519f6eb87db3be7a9effa8d04946a8ca0764b92
Tyga Kinsumbya <71526739+Rat-a-tail@users.noreply.github.com>
2023-05-13 11:46:34 -0500

    Add files via upload
    added src files

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1> {{ title }}</h1>
+
+  <Modal :header="header" :text ="text" :theme="sale"/>
+ <!-- <input type="text" ref="name">
+ <button @click="handleclick">click me</button> -->
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default { //root component to be exported
+  name: 'App',
+  components: {Modal},
+
+  data(){
+    return {
+      title: 'My first vue app',
+      header: 'Sign Up',
+      text:'this is a text'
+
+    }
+  },
+  /*methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }*/
+}
+</script>
+
+<style>
+#app { 
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
